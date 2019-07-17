0172e820d2b81c9041b7da5cfa163772212193e0
tal berry <talberry50@gmail.com>
2019-07-17 08:37:55 -0700

    Initial Commit

---
+++ Myfirstswiftapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Myfirstswiftapp
+//
+//  Created by Spur IQ on 7/17/19.
+//  Copyright © 2019 Markus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
