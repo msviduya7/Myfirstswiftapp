010d9114f61d186042df3030fc4626aa0bdb4c67
markus viduya <markusvid7479@gmail.com>
2019-07-17 12:43:22 -0700

    version 2

--- Myfirstswiftapp/ViewController.swift
+++ Myfirstswiftapp/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageview1: UIImageView!
+    var showfirstimage = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonclick(_ sender: Any) {
+        if (showfirstimage == false){
+         imageview1.image = UIImage(named:"Dice Red 3")
+            showfirstimage = true
+        }
+        else{
+        imageview1.image = UIImage(named:"Dice Red 6")
+            showfirstimage = false
+        }
+    }
 }
+
 
